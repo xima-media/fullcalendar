3f941e6737bcea29b4545902c80a2e7e8fa24da1
sean kenny <srkenny@gmail.com>
2014-09-09 10:39:38 +0100

    Adding new files

---
+++ lang/id.js
@@ -0,0 +1,10 @@
+$.fullCalendar.lang("id", {
+	defaultButtonText: {
+		month: "Bulan",
+		week: "Minggu",
+		day: "Hari",
+		list: "Agenda"
+	},
+	allDayHtml: "Sehari<br/>penuh",
+	eventLimitText: "lebih"
+});
