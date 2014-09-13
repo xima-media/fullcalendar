56219816c9e3b0be01625256d0050c210151edce
Sean Kenny <srkenny@gmail.com>
2014-09-13 12:55:01 +0100

    Day resource events now correctly assigned to resource column

--- src/resource/ResourceTimeGrid.events.js
+++ src/resource/ResourceTimeGrid.events.js
@@ -3,5 +3,31 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 
 $.extend(ResourceTimeGrid.prototype, {
+// Slices a single event into an array of event segments.
+  // When `intervalStart` and `intervalEnd` are specified, intersect the events with that interval.
+  // Otherwise, let the subclass decide how it wants to slice the segments over the grid.
+  eventToSegs: function(event, intervalStart, intervalEnd) {
+    var eventStart = event.start.clone().stripZone(); // normalize
+    var eventEnd = this.view.calendar.getEventEnd(event).stripZone(); // compute (if necessary) and normalize
+    var segs;
+    var i, seg;
 
+    if (intervalStart && intervalEnd) {
+      seg = intersectionToSeg(eventStart, eventEnd, intervalStart, intervalEnd);
+      segs = seg ? [ seg ] : [];
+    }
+    else {
+      segs = this.rangeToSegs(eventStart, eventEnd, event.resources); // defined by the subclass
+    }
+
+    // assign extra event-related properties to the segment objects
+    for (i = 0; i < segs.length; i++) {
+      seg = segs[i];
+      seg.event = event;
+      seg.eventStartMS = +eventStart;
+      seg.eventDurationMS = eventEnd - eventStart;
+    }
+
+    return segs;
+  }
 });
--- src/resource/ResourceTimeGrid.js
+++ src/resource/ResourceTimeGrid.js
@@ -9,5 +9,38 @@ function ResourceTimeGrid(view) {
 
 ResourceTimeGrid.prototype = createObject(TimeGrid.prototype); // define the super-class
 $.extend(ResourceTimeGrid.prototype, {
+// Slices up a date range into a segment for each column
+// Each column represents a resource.  An event can be assigned to multiple resources
+// so we need to build segs accordingly
+  rangeToSegs: function(rangeStart, rangeEnd, resourceIds) {
+    var view = this.view;
+    var segs = [];
+    var seg;
+    var col;
+    var cellDate;
+    var colStart, colEnd;
+
+    var resources = view.calendar.fetchResources();
+
+    // normalize
+    rangeStart = rangeStart.clone().stripZone();
+    rangeEnd = rangeEnd.clone().stripZone();
+
+    for (col = 0; col < view.colCnt; col++) {
+      var resource = resources[col];
+      if (resourceIds.indexOf(resource.id) > -1){
+        cellDate = view.cellToDate(0, col); // use the View's cell system for this
+        colStart = cellDate.clone().time(this.minTime);
+        colEnd = cellDate.clone().time(this.maxTime);
+        seg = intersectionToSeg(rangeStart, rangeEnd, colStart, colEnd);
+        if (seg) {
+          seg.col = col;
+          segs.push(seg);
+        }
+      }
+    }
+
+    return segs;
+  }
 
 });
