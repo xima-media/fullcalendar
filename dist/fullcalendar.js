06c746a13de50250ae07edb27b8c1d02c1148c8b
Sean Kenny <srkenny@gmail.com>
2014-09-23 18:57:40 +0100

    .

--- dist/fullcalendar.js
+++ dist/fullcalendar.js
@@ -1,7 +1,7 @@
 /*!
- * FullCalendar v2.1.1
- * Docs & License: http://arshaw.com/fullcalendar/
- * (c) 2014 Adam Shaw, Sean Kenny
+ * <%= meta.title %> v<%= meta.version %>
+ * Docs & License: <%= meta.homepage %>
+ * (c) <%= meta.copyright %>
  */
 
 (function(factory) {
@@ -174,7 +174,7 @@ var rtlDefaults = {
 
 ;;
 
-var fc = $.fullCalendar = { version: "2.1.1" };
+var fc = $.fullCalendar = { version: "<%= meta.version %>" };
 var fcViews = fc.views = {};
 
 
